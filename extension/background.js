0a7208cc85c871b351ea79e57b646d82cc98da34
Aravind <maravind2000@gmail.com>
2026-05-20 09:21:52 +0530

    Update URL for backend API

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://tubeshelf.vercel.app";
+const API_BASE = "https://tubeshelf-psi.vercel.app";
 
 async function getAccessToken(interactive = false) {
   return new Promise((resolve, reject) => {
